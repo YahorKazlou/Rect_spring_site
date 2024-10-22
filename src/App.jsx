b1d53a9e0412d4c5cb84e793cebe8f3e9f974246
Yahor Kazlou <yahor.kazlou.94@gmail.com>
2024-10-22 17:09:40 +0300

    Added CSS modules, refactoring index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import Navigation from "./components/Navigation";
-import MainBlock from "./components/MainBlock";
-import Projects from "./components/Projects";
+import Navigation from './components/Navigation';
+import MainBlock from './components/MainBlock';
+import Projects from './components/Projects';
+import styles from './App.module.css';
 
 function App() {
-      return (
-            <>
-                  <Navigation />
-                  <div className="main projects">
-                        <MainBlock />
-                        <Projects />
-                  </div>
-            </>
-      );
+    return (
+        <>
+            <Navigation />
+            <div className={styles.main}>
+                <MainBlock />
+                <Projects />
+            </div>
+        </>
+    );
 }
 
 export default App;
